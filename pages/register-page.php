5139b1e8a1142dbd935a74615f9165053b334c32
xarkanz <daffaarkana74@gmail.com>
2023-12-15 19:13:24 +0700

    register-page

--- pages/register-page.php
+++ pages/register-page.php
@@ -4,17 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log in | Vibe Flow</title>
-    <link rel="stylesheet" href="../style/login-style.css">
+    <link rel="stylesheet" href="../style/register-style.css">
 </head>
 <body>
     <div class="all-wrapper">
         <img src="../assets/images/logo-colorized.png" alt="" class="image-logo">
 
         <div class="contain-wrapper">
-            <h1>Login</h1>
+            <h1>Register</h1>
             <div class="field-wrapper">
                 <div class="textfield-wrapper">
                     <h2>Username</h2>
+                    <input type="text" class="text-field">
+                </div>
+                <div class="textfield-wrapper">
+                    <h2>Name</h2>
+                    <input type="text" class="text-field">
+                </div>
+                <div class="textfield-wrapper">
+                    <h2>Email</h2>
                     <input type="text" class="text-field">
                 </div>
                 <div class="textfield-wrapper">
@@ -28,18 +36,20 @@
                 </div>
                 <a href="#">
                     <div class="button-wrapper">
-                        <button class="login-button">Login</button>
+                        <button class="login-button">Register</button>
                     </div>
                 </a>
             </div>
             <div class="bottom-nav">
-                <h4 class="main-text-bottom">Don't have an account?</h4>
-                <a href="register-page.php">
-                    <h4 class="sec-text-bottom">Register</h4>
+                <h4 class="main-text-bottom">Already have an account?</h4>
+                <a href="login-page.php">
+                    <h4 class="sec-text-bottom">Login</h4>
                 </a>
             </div>
             <div class="or-wrapper">
+                <div class="line"></div>
                 <h4>OR</h4>
+                <div class="line"></div>
             </div>
             <a href="#">
                 <div class="google-wrapper">
