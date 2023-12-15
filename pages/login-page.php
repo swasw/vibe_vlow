f0103cfc8bdf12bafcc5e3fa2ff2eecaa8d407d4
xarkanz <daffaarkana74@gmail.com>
2023-12-15 17:30:08 +0700

    login page

---
+++ pages/login-page.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in | Vibe Flow</title>
+    <link rel="stylesheet" href="../style/login-style.css">
+</head>
+<body>
+    <div class="all-wrapper">
+        <img src="../assets/images/logo-colorized.png" alt="" class="image-logo">
+
+        <div class="contain-wrapper">
+            <h1>Login</h1>
+            <div class="field-wrapper">
+                <div class="textfield-wrapper">
+                    <h2>Username</h2>
+                    <input type="text" class="text-field">
+                </div>
+                <div class="textfield-wrapper">
+                    <h2>Password</h2>
+                    <input type="text" class="text-field">
+                    <div class="forgot-wrapper">
+                        <h3>Forgot Password?</h3>
+                    </div>
+                </div>
+                <a href="#">
+                    <div class="button-wrapper">
+                        <button class="login-button">Login</button>
+                    </div>
+                </a>
+            </div>
+            <div class="bottom-nav">
+                <h4 class="main-text-bottom">Don't have an account?</h4>
+                <a href="#">
+                    <h4 class="sec-text-bottom">Register</h4>
+                </a>
+            </div>
+            <a href="#">
+                <div class="google-wrapper">
+                    <img src="../assets/images/google (2).png" alt="" class="google-logo">
+                    <h5>Sign in with Google</h5>
+                </div>
+            </a>
+        </div>
+    </div>
+</body>
+</html>
