b0434512ac706782d295d711bbedefa3e3e6388e
smthl1keyou <145595637+smthl1keyou@users.noreply.github.com>
2024-01-01 17:28:25 +0700

    new regist_con

--- register_con.php
+++ register_con.php
@@ -1,26 +1,30 @@
 <?php
+
 session_start();
+
 require_once('dbcon.php'); 
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    try{
+
+    try {
         $insert_query = "INSERT INTO `user_data`(`username`, `password`, `name`, `email`) VALUES ('$username','$password','$name','$email')";
         $inserted = mysqli_query($connection, $insert_query);
-        header('Location: login-page.php');
-        exit();
-    }catch (Exception $e) {
-        echo 'username must be unique';
+
+        if ($inserted) {
+            $_SESSION['username'] = $username;
+
+            header('Location: login-page.php');
+            exit();
+        } else {
+            echo 'Registration failed';
+        }
+    } catch (Exception $e) {
+        echo 'Username must be unique';
         // echo "Error: " . $e->getMessage();
     }
-    // if($inserted){
-    //     header('Location: login-page.php');
-    //     exit();
-    // }
-    // else{
-    //     echo 'failed';
 }
-
 ?>
